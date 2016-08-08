332a8aa802d21762d2cb58289477dced0141e97b
Peng.Li <peng.li@rea-group.com>
2016-08-08 17:29:23 +0800

    jquery demo

--- public/entry.jsx
+++ public/entry.jsx
@@ -1,11 +1,15 @@
 import Hello from './hello.jsx';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
 
 ReactDOM.render(
   <Hello />,
   document.getElementById("content")
 );
+
+// use jquery
+console.log($('#content').text());
 
 // Notice!!!
 // Following is required to make reloading happen
