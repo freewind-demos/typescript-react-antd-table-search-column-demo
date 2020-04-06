2a9aeb1519d3c564cf9dcfc4af08c809a30c7793
Freewind <nowindlee@gmail.com>
2020-04-06 23:42:03 +0800

    typescript-react-antd-table-search-column-demo

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-TypeScript React AntDesign Table Demo
+TypeScript React AntDesign Table Search Column Demo
 =================================
+
+Antd的表格搜索功能设计得比较死板，需要很多配置才能使用，并且有两个特别两个特别不好用的地方：
+
+1. 没法做到边输边搜。因为必须调用`confirm`才会触发搜索，而它会自动关掉弹出来的搜索框
+2. 难以弹出搜索框后自动选中某个input，因为涉及到两个不同的方法。我们有两种办法解决这个问题，但都不完美：
+   1. 弄一个组件级别的ref，把search input保存在里面，跨方法使用。但如果有多个列，会乱，需要更复杂的管理方式
+   2. 每个column的定义弄成一个class，使用`this`来保存`searchInput`，多列不会乱，但是在定义时由于typescript的限制，每个方法签名必须写全，不能推断
+
+所以如果想实现一个更自由的Search，还需要想办法自己控制。
 
 ```
 npm install
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,52 +1,139 @@
-import React from 'react'
-import {Table} from 'antd';
+import {SearchOutlined} from '@ant-design/icons';
+import {Button, Input, Table} from 'antd';
+import {ColumnType, FilterDropdownProps} from 'antd/lib/table/interface';
+import React, {useRef} from 'react'
 
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    render: (text: string) => <a>{text}</a>,
-  },
-  {
-    title: 'Cash Assets',
-    className: 'column-money',
-    dataIndex: 'money',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-  },
-];
+type Data = {
+  key: string,
+  name: string
+}
 
-const data = [
+const data: Data[] = [
   {
     key: '1',
     name: 'John Brown',
-    money: '￥300,000.00',
-    address: 'New York No. 1 Lake Park',
   },
   {
     key: '2',
     name: 'Jim Green',
-    money: '￥1,256,000.00',
-    address: 'London No. 1 Lake Park',
   },
   {
     key: '3',
     name: 'Joe Black',
-    money: '￥120,000.00',
-    address: 'Sidney No. 1 Lake Park',
   },
-];
+]
 
 export default function Hello() {
+  const searchInputRef = useRef<Input | null>(null)
   return <div>
     <Table
-      columns={columns}
+      columns={[
+        {
+          title: 'Name',
+          dataIndex: 'name',
+          render: (text: string) => text,
+          filterDropdown: ({setSelectedKeys, selectedKeys, confirm, clearFilters}) => {
+            console.log("### filterDropdown", {setSelectedKeys, selectedKeys, confirm, clearFilters})
+            return <div style={{padding: 8}}>
+              <Input
+                ref={searchInputRef}
+                placeholder={'Search'}
+                value={selectedKeys[0]}
+                onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                onPressEnter={confirm}
+                style={{width: 188, marginBottom: 8, display: 'block'}}
+              />
+              <Button
+                type="primary"
+                onClick={confirm}
+                icon={<SearchOutlined/>}
+                size="small"
+                style={{width: 90, marginRight: 8}}
+              >
+                Search
+              </Button>
+              <Button size="small" style={{width: 90}} onClick={clearFilters}>
+                Reset
+              </Button>
+            </div>
+          },
+          filterIcon: filtered => <SearchOutlined style={{color: filtered ? '#1890ff' : undefined}}/>,
+          onFilter: (value, record) => {
+            console.log('### onFilter:', {value, record})
+            return record.name
+              .toString()
+              .toLowerCase()
+              .includes(value.toString().toLowerCase())
+          },
+          onFilterDropdownVisibleChange: (visible) => {
+            console.log("### onFilterDropdownVisibleChange", {visible});
+            if (visible) {
+              setTimeout(() => searchInputRef.current?.select());
+            }
+          },
+        },
+
+        new class implements ColumnType<Data> {
+          title = 'Name'
+          dataIndex = 'name'
+          render = (text: string) => text
+          searchInput: Input | null = null;
+
+          filterDropdown = ({setSelectedKeys, selectedKeys, confirm, clearFilters}: FilterDropdownProps) => {
+            console.log("### filterDropdown", {this: this, setSelectedKeys, selectedKeys, confirm, clearFilters})
+            return <div style={{padding: 8}}>
+              <Input
+                ref={node => {
+                  console.log("### ref", {node})
+                  this.searchInput = node
+                  console.log("### this", this);
+                }
+                }
+                placeholder={'Search'}
+                value={selectedKeys[0]}
+                onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                onPressEnter={confirm}
+                style={{width: 188, marginBottom: 8, display: 'block'}}
+              />
+              <Button
+                type="primary"
+                onClick={confirm}
+                icon={<SearchOutlined/>}
+                size="small"
+                style={{width: 90, marginRight: 8}}
+              >
+                Search
+              </Button>
+              <Button size="small" style={{width: 90}} onClick={clearFilters}>
+                Reset
+              </Button>
+            </div>
+          };
+
+          filterIcon = (filtered: boolean) => {
+            return <SearchOutlined style={{color: filtered ? '#1890ff' : undefined}}/>
+          }
+
+          onFilter = (value: string | number | boolean, record: Data) => {
+            console.log('### onFilter:', {value, record})
+            return record.name
+              .toString()
+              .toLowerCase()
+              .includes(value.toString().toLowerCase())
+          }
+
+          onFilterDropdownVisibleChange = (visible: boolean) => {
+            console.log("### onFilterDropdownVisibleChange", {visible});
+            if (visible) {
+              setTimeout(() => {
+                console.log("### onFilterDropdownVisibleChange.setTime", {this: this, visible});
+                this.searchInput?.select()
+              });
+            }
+          }
+        }
+      ]}
       dataSource={data}
-      bordered
-      title={() => 'Header'}
-      footer={() => 'Footer'}
     />
   </div>
 };
